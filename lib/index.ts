db1e1dea902720f31208f4a91769deb027e68e8c
cosycode <32601113+cosycode@users.noreply.github.com>
2018-06-07 11:11:15 +0200

    Fix TypeScript build system

---
+++ lib/index.ts
@@ -0,0 +1,18 @@
+/* *
+ * 
+ *  Copyright (c) Highsoft AS. All rights reserved.
+ * 
+ * */
+
+import * as colors from 'colors';
+
+require('colors');
+
+console.log();
+console.log('Highcharts Suite TypeScript Declaration Generator'.green.bold);
+console.log();
+console.log('Fetches API dumps from api.highcharts.com, and builds TypeScript .d files.');
+console.log('Note that API dumps are cached - delete the cache folder to force a refetch.');
+console.log();
+console.log('Declaration files ends up in dist/');
+console.log();
