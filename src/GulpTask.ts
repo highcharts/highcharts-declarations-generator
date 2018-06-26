1ff6583bfed6866faf09eaf60861cfa2d6b0f638
sophiebremer <40056287+sophiebremer@users.noreply.github.com>
2018-06-26 15:32:08 +0200

    Extend gulp task

--- src/GulpTask.ts
+++ src/GulpTask.ts
@@ -8,6 +8,9 @@ import * as colors from 'colors'; require('colors');
 import * as config from './Config';
 import * as namespaceGenerator from './NamespaceGenerator';
 import * as namespaceParser from './NamespaceParser';
+import * as optionsGenerator from './OptionsGenerator';
+import * as optionsParser from './OptionsParser';
+import * as tsd from './TypeScriptDeclarations';
 import * as utils from './Utilities';
 
 
@@ -18,10 +21,8 @@ export function task (done: Function) {
         'Started creating TypeScript declarations...'
     ));
 
-    return Promise
-        .all([
-            generateHighchartsNamespace()
-        ])
+    return generateHighchartsOptions()
+        .then(generateHighchartsNamespace)
         .then(() => console.log(colors.green.bold(
             'Finished creating TypeScript declarations.'
         )));
@@ -29,10 +30,25 @@ export function task (done: Function) {
 
 
 
-function generateHighchartsNamespace (): Promise<void> {
+function generateHighchartsOptions ():
+Promise<tsd.IDeclaration> {
+
+    return utils
+        .load(config.treeOptionsJsonPath)
+        .then(optionsParser.parse)
+        .then(optionsGenerator.generate);
+}
+
+
+
+function generateHighchartsNamespace (
+    optionsDeclarations: tsd.IDeclaration
+): Promise<void> {
 
     return utils
         .load(config.treeNamespaceJsonPath)
         .then(namespaceParser.splitIntoFiles)
-        .then(namespaceGenerator.saveIntoFiles);
+        .then(filesDictionary => namespaceGenerator
+            .saveIntoFiles(filesDictionary, optionsDeclarations)
+        );
 }  
