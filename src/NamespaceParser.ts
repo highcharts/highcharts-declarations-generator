a15e5e8a3497ec318761b395c8648871b2512314
sophiebremer <40056287+sophiebremer@users.noreply.github.com>
2018-09-06 13:35:13 +0200

    Improve namespace handling

--- src/NamespaceParser.ts
+++ src/NamespaceParser.ts
@@ -14,9 +14,8 @@ export function parseIntoFiles(json: any): Promise<utils.Dictionary<INode>> {
 
     return new Promise((resolve, reject) => {
 
-        let modules = new utils.Dictionary<INode>();
-
-        transferNodes(json, modules);
+        let modules = new utils.Dictionary<INode>(),
+            parser = new NamespaceParser(json, modules);
 
         resolve(modules);
     });
@@ -24,22 +23,190 @@ export function parseIntoFiles(json: any): Promise<utils.Dictionary<INode>> {
 
 
 
-/**
- * Copies all nodes into there modules.
- *
- * @param sourceNode
- *        The node to copy into the modules.
- *
- * @param targetModules
- *        The module dictionary to copy the node in.
- */
-function transferNodes (
-    sourceNode: INode, targetModules: utils.Dictionary<INode>
-) {
+class NamespaceParser {
 
-    if (sourceNode.doclet &&
-        sourceNode.meta
+    /* *
+     *
+     *  Static Functions
+     *
+     * */
+
+    /**
+     * Compares two light doclets for basic equality. Returns true, if the doclet is
+     * basically equal.
+     *
+     * @param docletA
+     *        First doclet to analyze.
+     *
+     * @param docletB
+     *        Second doclet to analyze.
+     */
+    private static isEqualDoclet (docletA: IDoclet, docletB: IDoclet) {
+
+        let nameA = tsd.IDeclaration.namespaces(docletA.name).join('.'),
+            nameB = tsd.IDeclaration.namespaces(docletB.name).join('.');
+
+        return (
+            nameA === nameB &&
+            (
+                Object.keys(docletA).length == 1 ||
+                Object.keys(docletB).length == 1 ||
+                utils.isDeepEqual(docletA, docletB)
+            )
+        );
+    }
+
+    /* *
+     *
+     *  Constructor
+     *
+     * */
+
+    /**
+     * Copies all nodes into there modules.
+     *
+     * @param sourceNode
+     *        The node to copy into the modules.
+     *
+     * @param targetModules
+     *        The module dictionary to copy the node in.
+     */
+    public constructor (
+        sourceNode: INode, targetModules: utils.Dictionary<INode>
     ) {
+
+        this._targetModules = targetModules;
+
+        this.transferNodes(sourceNode);
+    }
+
+    /* *
+     *
+     *  Properties
+     *
+     * */
+
+    public get targetModules (): utils.Dictionary<INode> {
+        return this._targetModules;
+    }
+    private _targetModules: utils.Dictionary<INode>;
+
+    /* *
+     *
+     *  Functions
+     *
+     * */
+
+    /**
+     * Search a node and returns it, if founded.
+     *
+     * @param rootNode
+     *        The root node to search in.
+     *
+     * @param nodeName
+     *        The full name of the node to find.
+     *
+     * @param overload
+     *        Create additional node.
+     */
+    private findNode (
+        rootNode: INode, nodeName: string, overload: boolean = false
+    ): INode {
+
+        let found = false,
+            node = rootNode,
+            spaceNames = tsd.IDeclaration.namespaces(nodeName, true),
+            indexEnd = (spaceNames.length - 1);
+
+        spaceNames.forEach((spaceName, index) => {
+
+            if (!node.children) {
+                node.children = [];
+            }
+
+            if (overload &&
+                index === indexEnd
+            ) {
+                found = false;
+            }
+            else {
+                found = node.children.some(child => {
+                    if (child.doclet.name === spaceName) {
+                        node = child;
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                });
+            }
+
+            if (!found) {
+
+                let newNode = {
+                    doclet: {
+                        name: spaceName
+                    }
+                } as INode;
+
+                node.children.push(newNode);
+
+                node = newNode;
+            }
+        });
+
+        return node;
+    }
+
+    /**
+     * Prepares and returns the specified module.
+     *
+     * @param modules
+     *        The dictionary with all modules.
+     *
+     * @param modulePath
+     *        The file path to the module.
+     */
+    private prepareModule (modulePath: string): INode {
+
+        let modules = this._targetModules;
+
+        modules[modulePath] = (modules[modulePath] || {
+            children: [],
+            doclet: {
+                description: '',
+                kind: 'global',
+                name: '',
+            },
+            meta: {
+                files: [{
+                    line: 0,
+                    path: modulePath + '.js'
+                }]
+            }
+        });
+
+        return modules[modulePath];
+    }
+
+    /**
+     * Copies all nodes into there modules.
+     *
+     * @param sourceNode
+     *        The node to copy into the modules.
+     */
+    private transferNodes (sourceNode: INode) {
+
+        if (!sourceNode.doclet ||
+            !sourceNode.meta
+        ) {
+            if (sourceNode.children) {
+                sourceNode.children.forEach(
+                    sourceChild => this.transferNodes(sourceChild)
+                );
+            }
+            return;
+        }
 
         let sourceDoclet = sourceNode.doclet,
             sourceMeta = sourceNode.meta;
@@ -48,158 +215,36 @@ function transferNodes (
             .map(file => utils.base(file.path))
             .forEach(modulePath => {
 
-            let moduleNode = prepareModule(targetModules, modulePath),
-                targetName = (sourceDoclet.name || ''),
-                targetNode = findNode(moduleNode, targetName),
-                targetDoclet = targetNode.doclet;
-
-            if (targetDoclet &&
-                !isEqual(targetDoclet, sourceDoclet)
-            ) {
-                targetNode = findNode(moduleNode, targetName, true);
-                targetDoclet = targetNode.doclet;
-            }
-
-            if (!targetDoclet) {
-                targetDoclet = targetNode.doclet = {} as any;
-            }
-
-            Object
-                .keys(sourceDoclet)
-                .forEach(key =>
-                    (targetDoclet as any)[key] = (sourceDoclet as any)[key]
-                );
-        });
-    }
-
-    if (sourceNode.children) {
-        sourceNode.children.forEach(
-            sourceChild => transferNodes(sourceChild, targetModules)
-        );
-    }
-}
-
-
-
-/**
- * Compares two light doclets for basic equality. Returns true, if the doclet is
- * basically equal.
- *
- * @param docletA
- *        First doclet to analyze.
- *
- * @param docletB
- *        Second doclet to analyze.
- */
-function isEqual (docletA: IDoclet, docletB: IDoclet) {
-
-    let nameA = tsd.IDeclaration.namespaces(docletA.name).join('.'),
-        nameB = tsd.IDeclaration.namespaces(docletB.name).join('.');
-
-    return (
-        nameA === nameB &&
-        (
-            Object.keys(docletA).length == 1 ||
-            Object.keys(docletB).length == 1 ||
-            utils.isDeepEqual(docletA, docletB)
-        )
-    );
-}
-
-
-
-/**
- * Search a node and returns it, if founded.
- *
- * @param rootNode
- *        The root node to search in.
- *
- * @param nodeName
- *        The full name of the node to find.
- *
- * @param overload
- *        Create additional node.
- */
-function findNode (
-    rootNode: INode, nodeName: string, overload: boolean = false
-): INode {
-
-    let found = false,
-        node = rootNode,
-        spaceNames = tsd.IDeclaration.namespaces(nodeName, true),
-        indexEnd = (spaceNames.length - 1);
-
-    spaceNames.forEach((spaceName, index) => {
-
-        if (!node.children) {
-            node.children = [];
-        }
-
-        if (overload &&
-            index === indexEnd
-        ) {
-            found = false;
-        }
-        else {
-            found = node.children.some(child => {
-                if (child.doclet.name === spaceName) {
-                    node = child;
-                    return true;
+                let moduleNode = this.prepareModule(modulePath),
+                    targetName = (sourceDoclet.name || ''),
+                    targetNode = this.findNode(moduleNode, targetName),
+                    targetDoclet = targetNode.doclet;
+                
+                if (targetDoclet &&
+                    !NamespaceParser.isEqualDoclet(targetDoclet, sourceDoclet)
+                ) {
+                    targetNode = this.findNode(moduleNode, targetName, true);
+                    targetDoclet = targetNode.doclet;
                 }
-                else {
-                    return false;
+
+                if (!targetDoclet) {
+                    targetDoclet = targetNode.doclet = {} as any;
                 }
+
+                Object
+                    .keys(sourceDoclet)
+                    .forEach(key =>
+                        (targetDoclet as any)[key] = (sourceDoclet as any)[key]
+                    );
             });
+
+        if (sourceNode.children) {
+            sourceNode.children.forEach(
+                sourceChild => this.transferNodes(sourceChild)
+            );
         }
+    }
 
-        if (!found) {
-
-            let newNode = {
-                doclet: {
-                    name: spaceName
-                }
-            } as INode;
-
-            node.children.push(newNode);
-
-            node = newNode;
-        }
-    });
-
-    return node;
-}
-
-
-
-/**
- * Prepares and returns the specified module.
- *
- * @param modules
- *        The dictionary with all modules.
- *
- * @param modulePath
- *        The file path to the module.
- */
-function prepareModule (
-    modules: utils.Dictionary<INode>, modulePath: string
-): INode {
-
-    modules[modulePath] = (modules[modulePath] || {
-        children: [],
-        doclet: {
-            description: '',
-            kind: 'global',
-            name: '',
-        },
-        meta: {
-            files: [{
-                line: 0,
-                path: modulePath + '.js'
-            }]
-        }
-    });
-
-    return modules[modulePath];
 }
 
 
